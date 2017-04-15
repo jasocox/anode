922b0c61ba4e4d02de147602a7cc75e55adde095
Jason Cox <JasonWilburCox@gmail.com>
2017-04-15 18:08:39 -0400

    Basic server

--- server.js
+++ server.js
@@ -2,4 +2,8 @@ var express = require('express');
 var app = express();
 var config = require('./config');
 
-console.log('Config:', config);
+app.use(express.static(config.public));
+
+app.listen(config.port, function() {
+  console.log('Started server at', config.port);
+});
